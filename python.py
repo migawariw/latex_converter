724320a63ffde12d7813cadf9e4862f63f0dcdc9
migawariw <112729656+migawariw@users.noreply.github.com>
2023-04-04 09:25:16 +0900

    Create python.py

---
+++ python.py
@@ -0,0 +1,31 @@
+import requests
+
+# GitHubリポジトリの情報
+username = "your_github_username"
+repository = "your_repository_name"
+branch = "main"  # or whichever branch you want to use
+path = "path/to/save/latex/images"  # the directory in the repo where the images will be saved
+token = "your_github_personal_access_token"  # this token must have repo access
+
+# LaTeX数式を画像に変換するAPIのエンドポイント
+api_endpoint = "https://latex.codecogs.com/png.latex"
+
+# LaTeX数式
+latex_formula = "your_latex_formula"
+
+# APIリクエストを送信して画像を取得する
+response = requests.get(api_endpoint, params={"code": latex_formula})
+
+# 取得した画像をGitHubリポジトリにアップロードする
+url = f"https://api.github.com/repos/{username}/{repository}/contents/{path}/{latex_formula}.png"
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json",
+    "Accept": "application/vnd.github.v3+json",
+}
+data = {
+    "message": f"Add image of {latex_formula}",
+    "content": response.content.decode("utf-8"),
+    "branch": branch,
+}
+response = requests.put(url, headers=headers, json=data)
